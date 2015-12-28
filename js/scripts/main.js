8becd81fa23854d5caca326e8d0ef063691632e9
manikandan <manikandan@Sudhakars-iMac.local>
2015-12-28 16:44:42 +0530

    project setup

---
+++ js/scripts/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	var windowHeight = $( window ).innerHeight();
+    $('.home-bg-img').css( "height", windowHeight);
+    $('.home-banner').css( "height", windowHeight - 99);
+
+});
